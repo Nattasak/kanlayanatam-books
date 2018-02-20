efc4bee80fc40163f679064e09f997eaf8c70516
Nattasak <dottedes@gmail.com>
2018-02-21 02:28:57 +0700

    add size props to book-item component

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -5,34 +5,36 @@ import "./BookItem.css"
 
 export default class BookItem extends React.Component {
   render() {
-    const { author, children, img, pdf, star, title } = this.props
+    const { author, children, img, pdf, size, star, title } = this.props
     return (
-      <article className="tile is-child box">
-        <figure className="image">
-          <a
-            href={pdf}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hvr-rotate"
-          >
-            <img src={img} alt={title} />
-          </a>
-        </figure>
-        <Rate disabled value={star} />
-        <div className="is-pulled-right">
-          <a href={pdf} target="_blank" rel="noopener noreferrer">
-            <Tooltip title="PDF">
-              <Icon type="download" style={{ fontSize: 27 }} />
-            </Tooltip>
-          </a>
-        </div>
-        <div className="tag-wrap">
-          <Tag>{author}</Tag>
-        </div>
-        <div className="content">
-          <p>{children}</p>
-        </div>
-      </article>
+      <div className={`tile is-parent is-${size}`}>
+        <article className="tile is-child box">
+          <figure className="image">
+            <a
+              href={pdf}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hvr-rotate"
+            >
+              <img src={img} alt={title} />
+            </a>
+          </figure>
+          <Rate disabled value={star} />
+          <div className="is-pulled-right">
+            <a href={pdf} target="_blank" rel="noopener noreferrer">
+              <Tooltip title="PDF">
+                <Icon type="download" style={{ fontSize: 27 }} />
+              </Tooltip>
+            </a>
+          </div>
+          <div className="tag-wrap">
+            <Tag>{author}</Tag>
+          </div>
+          <div className="content">
+            <p>{children}</p>
+          </div>
+        </article>
+      </div>
     )
   }
 }
