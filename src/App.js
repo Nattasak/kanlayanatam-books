9457bc9777a3e16bcace072a22530fcf0e907ebb
Nattasak <dottedes@gmail.com>
2018-02-20 19:58:32 +0700

    add header

--- src/App.js
+++ src/App.js
@@ -5,15 +5,23 @@ import BookList from "./components/BookList"
 
 import "./App.css"
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
+        <section className="hero">
+          <div className="hero-body">
+            <div className="container">
+              <a href="/">
+                <h1 className="title">ชมรมกัลยาณธรรม</h1>
+                <h2 className="subtitle">Dhamma books</h2>
+              </a>
+            </div>
+          </div>
+        </section>
         <BookList />
         <BackTop />
       </div>
     )
   }
 }
-
-export default App
