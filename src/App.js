ee3375968c9936fab5cdd8765a84e082fe9d775c
Nattasak <dottedes@gmail.com>
2018-02-20 17:08:09 +0700

    add back to top

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react"
+import { BackTop } from "antd"
+
 import "./App.css"
 
-import BookList from './components/BookList';
+import BookList from "./components/BookList"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <BookList />
+        <BackTop />
       </div>
     )
   }
