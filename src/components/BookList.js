6602ea1245f0be4cd237d6262fd568dfbfb56d45
Nattasak <dottedes@gmail.com>
2018-02-23 20:11:35 +0700

    sort authors

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -9,55 +9,20 @@ import BookItem from "./BookItem"
 import books from "../books.json"
 
 const CheckableTag = Tag.CheckableTag
-const tagsFromServer = uniq(books.map(b => b.author))
 
-/* not use ( use new way ^^ ~ )
-function BooksRowsRenderOldWay() {
-  const c = 4
-  var rows = []
-
-  for (let i = 0; i < books.length; i += c) {
-    const row = books.slice(i, i + c)
-    const booksRow = row.map((item, i) => (
-      <BookItem
-        key={i}
-        title={item.title}
-        author={item.author}
-        img={item.img}
-        pdf={item.pdf}
-        caption={item.caption}
-        star={item.star}
-      />
-    ))
-    rows.push(booksRow)
-  }
-
-  const booksRows = rows.map((row, i) => (
-    <div key={i} className="tile is-ancestor">
-      {row}
-    </div>
-  ))
-  return booksRows
-} */
+const tagsFromServer = uniq(books.map(b => b.author)).sort()
 
 function BooksRowsRender({ authors }) {
-  let data = books
+  let booksFiltered = books
 
   if (authors.length) {
-    const booksFilterByAuthor = authors.map(a => {
+    const booksFilterByAuthors = authors.map(a => {
       return books.filter(b => b.author === a)
     })
-    data = flattenDeep(booksFilterByAuthor)
+    booksFiltered = flattenDeep(booksFilterByAuthors)
   }
 
-  ///////////////////////////////////////////////////
-  // before chunk: [ {}, {}, {}, {}, {}, {} ]
-  // how to render?: map item (obj) ได้เลย!
-
-  // after chunk: [ [{}, {}], [{}, {}], [{}, {}] ]
-  // how to render?: map ทีละก้อน (arr) -> แล้วค่อย map item (obj) ในแต่ละก้อน
-
-  const booksChunk = chunk(data, 4)
+  const booksChunk = chunk(booksFiltered, 4)
 
   const result = booksChunk.map((row, i) => {
     const booksRow = row.map((b, i) => (
